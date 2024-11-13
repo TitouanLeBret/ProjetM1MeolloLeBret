19a06298f15560f85f33a64e58aecee65f3cae5f
TitouanLeBret <154740047+TitouanLeBret@users.noreply.github.com>
2024-11-13 10:08:23 +0100

    Modifs site
    TEst avec tailwind css

--- ProjetWEB/SiteCourse/src/siteCourse/views.py
+++ ProjetWEB/SiteCourse/src/siteCourse/views.py
@@ -4,6 +4,21 @@ def accueil(request):
     return render(request, 'accueil.html')
 
 def inscriptions(request):
+    """if request.method == "POST":
+        nom = request.POST['nom']
+        prenom = request.POST['prenom']
+        email = request.POST['email']
+        age = request.POST['age']
+        course = request.POST['course']
+
+        # Crée une nouvelle instance de l'inscription
+        inscription = Inscription(nom=nom, prenom=prenom, email=email, age=age, course=course)
+        inscription.save()
+
+        # Redirige vers une page de confirmation
+        return redirect('confirmation')
+
+    return render(request, 'inscriptions.html')"""
     return render(request, 'inscriptions.html')
 
 def parcours(request):
