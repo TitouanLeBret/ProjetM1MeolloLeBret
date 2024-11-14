a9394e77b4a20fc6495941db50edb9c2307fa173
TitouanLeBret <154740047+TitouanLeBret@users.noreply.github.com>
2024-11-14 14:25:40 +0100

    application css sur toutes les pages, jolie rendu frontend
    Tailwind css, super pratique, on peut appliquer des thème facilement, voir le site officiel pour tous les parametre, juste rajouté dans le squelette.html : <script src="https://cdn.tailwindcss.com"></script>, et après ça marche sur toutes les pages car elles l'include.

---
+++ ProjetWEB/SiteCourse/src/siteCourse/functions.py
@@ -0,0 +1,13 @@
+def Inscription(nom, prenom, email, age, course) :
+    """
+    Cette fonction permet d'inscrire un utilisateur dans une base de données.
+    :param nom: Le nom de l'utilisateur.
+    :param prenom: Le prénom de l'utilisateur.
+    :param email: L'adresse email de l'utilisateur.
+    :param age: L'âge de l'utilisateur.
+    :param course: Le nom du cours s'il y en a un.
+    """
+    # Code pour inscrire l'utilisateur dans la base de données
+    # Utiliser votre propre base de données ou une bibliothèque adaptée
+    # Pour l'instant affiche juste les infos rentré
+    pass
--- ProjetWEB/SiteCourse/src/siteCourse/views.py
+++ ProjetWEB/SiteCourse/src/siteCourse/views.py
@@ -1,24 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def accueil(request):
     return render(request, 'accueil.html')
 
 def inscriptions(request):
-    """if request.method == "POST":
+    if request.method == "POST":
         nom = request.POST['nom']
         prenom = request.POST['prenom']
         email = request.POST['email']
         age = request.POST['age']
         course = request.POST['course']
 
-        # Crée une nouvelle instance de l'inscription
-        inscription = Inscription(nom=nom, prenom=prenom, email=email, age=age, course=course)
-        inscription.save()
+        # Crée une nouvelle instance de l'inscription dans la BDD
+        #inscription = Inscription(nom=nom, prenom=prenom, email=email, age=age, course=course)
+        #inscription.save()
 
         # Redirige vers une page de confirmation
-        return redirect('confirmation')
-
-    return render(request, 'inscriptions.html')"""
+        context = {
+            'nom': nom,
+            'prenom': prenom,
+            'email': email,
+            'age': age,
+            'course': course,
+            'inscription_complete': True  # Indicateur pour afficher le message
+        }
+        return render(request, 'inscriptions.html', context)
     return render(request, 'inscriptions.html')
 
 def parcours(request):
