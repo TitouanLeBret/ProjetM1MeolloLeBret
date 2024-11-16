a9884119b78f7053b359fe605cdde29390606d70
TitouanLeBret <154740047+TitouanLeBret@users.noreply.github.com>
2024-11-16 15:11:47 +0100

    oublie de inscrriptions/urls.py

---
+++ ProjetWEB/SiteCourse/src/inscriptions/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from siteCourse.views import inscriptions
+
+
+app_name = "inscriptions"
+
+urlpatterns = [
+    path('', inscriptions , name='home'),
+    path('insc_complete/', views.insc_complete , name='complete'),
+    path('insc_failed/', views.insc_failed , name='failed'),
+    path('paiement/', views.paiement , name='paiement'),
+    #path('mdpchange,.../......) etc
+]
